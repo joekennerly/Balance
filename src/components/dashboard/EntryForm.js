4a86caf6205bcd3ec4c666689823671890f99d93
joekennerly <joseph.kennerly@gmail.com>
2019-07-28 17:14:08 -0500

    fix header, use compDidMo to load current date and category

--- src/components/dashboard/EntryForm.js
+++ src/components/dashboard/EntryForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
+import APIManager from "../../modules/APIManager"
 
 export default class EntryForm extends Component {
-
   state = {
     date: "",
     category: "",
@@ -9,53 +9,71 @@ export default class EntryForm extends Component {
     amount: ""
   }
 
+  //Load current date and current category
+  componentDidMount() {
+    const stateToChange = {}
+    stateToChange.date = this.props.date
+    stateToChange.category = document.querySelector("#category").value
+    this.setState(stateToChange)
+  }
+
+  //Save current value when changed
   handleFieldChange = event => {
     const stateToChange = {}
     stateToChange[event.target.id] = event.target.value
     this.setState(stateToChange)
   }
 
+  //Handle Submit
   handleClick = () => {
-    console.log(this.state)
+    // Post to db
+    APIManager.post("expenses", this.state)
+    this.props.history.push("/")
   }
 
   render() {
     return (
       <React.Fragment>
         <div className="ui form">
-          <div className="three fields">
+          <div className="five fields">
             <div className="field">
               <input
                 id="date"
                 type="date"
+                value={this.props.date}
                 onChange={this.handleFieldChange}
-                placeholder="date" />
+              />
             </div>
             <div className="field">
-              <input
+              <select
                 id="category"
                 type="text"
+                placeholder="category"
                 onChange={this.handleFieldChange}
-                placeholder="category" />
+              >
+                <option value="food">Food</option>
+                <option value="utilities">Utilities</option>
+                <option value="car">Car</option>
+                <option value="health">Health</option>
+              </select>
             </div>
             <div className="field">
               <input
                 id="name"
                 type="text"
                 onChange={this.handleFieldChange}
-                autoFocus placeholder="expense" />
+                placeholder="expense"
+              />
             </div>
             <div className="field">
               <input
                 id="amount"
                 type="text"
                 onChange={this.handleFieldChange}
-                placeholder="amount" />
+                placeholder="amount"
+              />
             </div>
-            <button
-              className="ui button"
-              onClick={this.handleClick}
-            >
+            <button className="ui button" onClick={this.handleClick}>
               <i className="plus icon" />
             </button>
           </div>
--- src/components/dashboard/Expenses.js
+++ src/components/dashboard/Expenses.js
@@ -6,26 +6,33 @@ export default class Expenses extends Component {
   render() {
     return (
       <React.Fragment>
-        <section className="expenses ui six column grid">
+        <section className="expenses ui five column grid">
           <div className="row card">
-            <div className="column"><b>Date</b></div>
-            <div className="column"><b>Category</b></div>
-            <div className="column"><b>Name</b></div>
-            <div className="column"><b>Amount</b></div>
-          </div>
-          <div className="row card">
-            <EntryForm />
-          </div>
-          {this.props.expenses.map(expense => (
-            <div key={expense.id} className="row card">
-              <div className="column">{expense.date}</div>
-              <div className="column">{expense.category}</div>
-              <div className="column">{expense.name}</div>
-              <div className="column">{expense.amount}</div>
-              <button className="ui button">edit</button>
-              <button className="ui button">delete</button>
+            <div className="column">
+              <b>Date</b>
             </div>
-          ))}
+            <div className="column">
+              <b>Category</b>
+            </div>
+            <div className="column">
+              <b>Name</b>
+            </div>
+            <div className="column">
+              <b>Amount</b>
+            </div>
+          </div>
+          <div className="row card">
+            <EntryForm {...this.props} />
+          </div>
+            {this.props.expenses.map(expense => (
+              <div key={expense.id} className="row card">
+                <div className="column">{expense.date}</div>
+                <div className="column">{expense.category}</div>
+                <div className="column">{expense.name}</div>
+                <div className="column">{expense.amount}</div>
+                <button className="ui button">x</button>
+              </div>
+            ))}
         </section>
       </React.Fragment>
     )
