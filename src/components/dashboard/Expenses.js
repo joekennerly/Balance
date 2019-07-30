d4ca9cf9134746965734ba9df6df3d67047b214a
joekennerly <joseph.kennerly@gmail.com>
2019-07-30 14:33:46 -0500

    toggling the edit form updates state with current selection

--- src/components/dashboard/Expenses.js
+++ src/components/dashboard/Expenses.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import EntryForm from "./EntryForm"
+// import APIManager from "../../modules/APIManager"
 
 export default class Expenses extends Component {
   state = {
@@ -11,32 +12,33 @@ export default class Expenses extends Component {
 
   //Save current value when changed
   handleKeyPress = event => {
-    console.log(event)
-    if (event.key === "Enter") {
-      console.log("enter")
-    }
-    else {
-      const stateToChange = {}
-      stateToChange[event.target.id] = event.target.value
-      this.setState(stateToChange)
+    const stateToChange = {}
+    stateToChange[event.target.id] = event.target.value
+    this.setState(stateToChange)
+  }
+
+  handleClick = event => {
+    if (!event.target.id.startsWith("edit")) {
+      //show edit form when text is clicked
+      let eventId = +event.target.id.split("-")[1]
+      event.target.children[0].classList.toggle("hide")
+      //find the object with matching id from this.props
+      let upObj = this.props.expenses.find(expense => expense.id === eventId)
+      //update state with current values
+      this.setState(upObj)
+    } else {
+      //Do nothing if edit-input is clicked
+      event.preventDefault()
     }
   }
 
-  handleClick = (e) => {
-    if (!e.target.id.startsWith("edit")) {
-      e.target.children[0].classList.toggle("hide")
-      e.target.children[0].autofocus = true
-
-      // let simpleId = e.target.id.split("-")[0]
-      const newState = {}
-      newState[e.target.id] = e.target.value
-      this.setState(newState)
-
-      console.log(e.target.id, this.state)
-    }
-    else {
-      //Do nothing if edit-input is clicked
-      return e.preventDefault()
+  enterKey = (event) => {
+    if (event.key === "Enter") {
+      console.log(this.state)
+      // console.log("enter", +event.target.id.split("-")[2])
+      // let eventId = +event.target.id.split("-")[2]
+      // let updatedObject = {}
+      // return this.props.updateItem("expenses", eventId, updatedObject, "/dashboard")
     }
   }
 
@@ -64,46 +66,74 @@ export default class Expenses extends Component {
 
           {this.props.expenses.map(expense => (
             <div key={expense.id} className="row card">
-              <div id={`date-${expense.id}`} className="column" onClick={this.handleClick}>
+              <div
+                id={`date-${expense.id}`}
+                className="column"
+                onClick={this.handleClick}
+              >
                 {expense.date}
-                <input id={`edit-date-${expense.id}`}
+                <input
+                  id={`edit-date-${expense.id}`}
                   type="date"
-                  value={this.state.date}
+                  placeholder={expense.date}
                   className="hide"
                   onChange={this.handleKeyPress}
+                  onKeyPress={this.enterKey}
                 />
               </div>
-              <div id={`category-${expense.id}`} className="column" onClick={this.handleClick}>
+              <div
+                id={`category-${expense.id}`}
+                className="column"
+                onClick={this.handleClick}
+              >
                 {expense.category}
-                <input id={`edit-category-${expense.id}`}
+                <input
+                  id={`edit-category-${expense.id}`}
                   type="text"
-                  value={this.state.category}
+                  placeholder={expense.category}
                   className="hide"
                   onChange={this.handleKeyPress}
+                  onKeyPress={this.enterKey}
                 />
               </div>
-              <div id={`name-${expense.id}`} className="column" onClick={this.handleClick}>
+              <div
+                id={`name-${expense.id}`}
+                className="column"
+                onClick={this.handleClick}
+              >
                 {expense.name}
-                <input id={`edit-name-${expense.id}`}
+                <input
+                  id={`edit-name-${expense.id}`}
                   type="text"
-                  value={this.state.name}
+                  placeholder={expense.name}
                   className="hide"
                   onChange={this.handleKeyPress}
+                  onKeyPress={this.enterKey}
                 />
               </div>
-              <div id={`amount-${expense.id}`} className="column" onClick={this.handleClick}>
+              <div
+                id={`amount-${expense.id}`}
+                className="column"
+                onClick={this.handleClick}
+              >
                 {expense.amount}
-                <input id={`edit-amount-${expense.id}`}
+                <input
+                  id={`edit-amount-${expense.id}`}
                   type="text"
-                  value={this.state.amount}
+                  placeholder={expense.amount}
                   className="hide"
                   onChange={this.handleKeyPress}
+                  onKeyPress={this.enterKey}
                 />
               </div>
               <button
                 className="ui button"
-                onClick={()=>this.props.deleteItem("expenses", expense.id, "/dashboard")}
-              >x</button>
+                onClick={() =>
+                  this.props.deleteItem("expenses", expense.id, "/dashboard")
+                }
+              >
+                x
+              </button>
             </div>
           ))}
         </section>
